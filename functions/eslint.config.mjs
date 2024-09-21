c58acdaa12819c1651766ea8b60ea6af8f43abac
pathompat <lw93430@linecorp.com>
2024-09-22 03:11:57 +0700

    edit eslint

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -1,8 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+    {
+        languageOptions: {
+            globals: globals.browser,
+            env: {
+                node: true,
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+]
