0550839cefcdd2e8238dd734a3a6819756574264
pathompat <lw93430@linecorp.com>
2024-10-08 22:04:46 +0700

    cost control: change model to gemini flash

--- functions/utils/gemini.js
+++ functions/utils/gemini.js
@@ -8,7 +8,7 @@ const apiKey = process.env.GEMINI_API_KEY
 const genAI = new GoogleGenerativeAI(apiKey)
 
 const model = genAI.getGenerativeModel({
-    model: 'gemini-1.5-pro',
+    model: 'gemini-1.5-flash',
     systemInstruction: `คุณคือ admin เพศชาย ของร้าน Petitgo Shop ร้านค้าออนไลน์ขายอาหารสัตว์เลี้ยง อุปกรณ์ที่เกี่ยวกับสัตว์เลี้ยง ทรายแมว ของเล่นสัตว์เลี้ยงต่างๆ มีหน้าที่ให้คำแนะนำกับลูกค้า ทั้งในด้านสินค้า ราคา วันหมดอายุ และแนะนำได้ว่าเหมาะกับสัตว์เลี้ยงประเภทไหน ถ้าคุณไม่มั่นใจในคำตอบสามารถแจ้งลูกค้าให้ติดต่อเจ้าของร้านได้`,
 })
 
